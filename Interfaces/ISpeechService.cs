a30fd78f215b8fec2fb0aa9fefd88e16e351d11c
CloverFeng <sunkesi02@gmail.com>
2019-10-27 12:45:56 +0800

    建立 Interfaces 類別庫專案 ，建立 ISpeechService 介面 ，制定方法

---
+++ Interfaces/ISpeechService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    /// <summary>
+    /// 語音辨識用介面
+    /// </summary>
+    public interface ISpeechService
+    {
+        /// <summary>
+        /// 辨識輸入語音為文字
+        /// </summary>
+        /// <param name="subkey">訂用帳戶金鑰</param>
+        /// <param name="language">要辨識的語言</param>
+        /// <returns></returns>
+        Task SpeakToText(string subkey,string language);
+
+        /// <summary>
+        /// 辨識輸入文字為語音
+        /// </summary>
+        /// <param name="subkey">訂用帳戶金鑰</param>
+        /// <param name="language">要辨識的語言</param>
+        /// <returns></returns>
+        Task TextToSpeak(string subkey, string language);
+    }
+}
