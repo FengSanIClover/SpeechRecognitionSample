6e47c317c454ece51b61b8283d7b3cf312eff56f
CloverFeng <sunkesi02@gmail.com>
2019-10-27 12:38:48 +0800

    加入專案檔案。

---
+++ SpeechRecognitionSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeechRecognitionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
